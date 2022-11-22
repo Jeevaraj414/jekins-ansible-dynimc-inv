e949de43bf82918cc8ae2532aa092d6c2b46e1a6
JEEVARAJ R <114459983+Jeevaraj414@users.noreply.github.com>
2022-11-22 13:29:57 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,16 +1,15 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+   region  = "ap-south-1" 
    
 }
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
-     ami     =  "ami-051f0947e420652a9"
+     ami     =  "ami-019774e5caffd1685"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     key_name      = "jeevarajdevops"
      tags = {
         Name = "tomcatserver"
      }
 }
-
